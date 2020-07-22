5a62f4d410f6d2ca76af9aa1bf0eb26a9ba7a646
CodingNoye <codingnoye@kookmin.ac.kr>
2020-07-22 23:49:36 +0900

    0722

--- 2805.cpp
+++ 2805.cpp
@@ -10,18 +10,26 @@ int main() {
     int N;
     ull M;
     cin>>N>>M;
-    for (int i=0; i<N; i++) cin>>tree[i];
+    ull MX;
+    for (int i=0; i<N; i++) {
+        cin>>tree[i];
+        MX = (MX>tree[i])?MX:tree[i];
+    }
     ull l=0;
-    ull r = 2000000001;
-    while (l<r) {
+    ull r = MX;
+    int max_height = 0;
+    while (l<=r) {
         const ull m = (l+r)/2;
         ull now=0;
         for (int i=0; i<N; i++) {
             now += (m<=tree[i])?tree[i]-m:0;
         }
         //cout<<m<<" "<<now<<endl;
-        if (now<=M) r = m-1;
-        else l = m+1;
+        if (now<M) r = m-1;
+        else {
+            max_height = max_height>=m?max_height:m;
+            l = m+1;
+        }
     }
-    cout<<l<<'\n';
+    cout<<max_height<<'\n';
 }
