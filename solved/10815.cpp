1129a2a37081b45e7fc27b5aa1142bbe62a514cd
CodingNoye <codingnoye@kookmin.ac.kr>
2020-09-22 09:36:08 +0900

    0922

--- solved/10815.cpp
+++ solved/10815.cpp
@@ -9,6 +9,8 @@ using namespace std;
 int arr[500001];
 
 int main() {
+    ios_base::sync_with_stdio(false);
+    cin.tie(0);
     int N; cin>>N;
     for (int i=0; i<N; i++) cin>>arr[i];
     sort(arr, arr+N);
