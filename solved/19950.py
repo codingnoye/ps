162e8600ffc145cd8dcc555c1329d318c5a01bc0
CodingNoye <codingnoye@kookmin.ac.kr>
2020-10-03 21:02:45 +0900

    1003

--- solved/19950.py
+++ solved/19950.py
@@ -1,5 +1,4 @@
-a,b,c,d,e,f = map(int, input().split())
+a,b,c,d,e,f=map(int,input().split())
 input()
-g = [*map(int,input().split())] + [round(((d-a)**2+(e-b)**2+(f-c)**2)**.5, 2)]
-if max(g) <= sum(g)-max(g): print("YES")
-else: print("NO")
+g=[*map(int,input().split())]+[((d-a)**2+(e-b)**2+(f-c)**2)**.5]
+print("YES"if max(g)*2<=sum(g)else"NO")
