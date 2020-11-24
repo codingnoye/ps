a00c605f121ec87326183c13060111db03f58b88
CodingNoye <codingnoye@kookmin.ac.kr>
2020-11-24 13:43:24 +0900

    1124

--- solved/1766.cpp
+++ solved/1766.cpp
@@ -10,8 +10,8 @@ typedef pair<int, int> ii;
 typedef pair<ull, ull> ll;
 
 vector<vector<int>> graph;
-int in_degree[1000];
-bool visited[1000];
+int in_degree[32000];
+bool visited[32000];
 
 void after(int a, int b) {
     graph[a].push_back(b);
@@ -22,32 +22,28 @@ void solve() {
     int N, M; cin>>N>>M;
     graph.resize(N);
 
-    for (int n=0; n<M; n++) {
-        int c; cin>>c;
-        int last, now; cin>>now;
-        for (int i=0; i<c-1; i++) {
-            last = now;
-            cin>>now;
-            after(last-1, now-1);
-        }
+    for (int m=0; m<M; m++) {
+        int a, b; cin>>a>>b;
+        after(a-1, b-1);
     }
 
     vector<int> res;
-    queue<int> q;
+    priority_queue<int, vector<int>, greater<int>> pq;
     for (int i=0; i<N; i++) {
-        if (in_degree[i] == 0) q.push(i);
+        if (in_degree[i] == 0) pq.push(i);
     }
-    while (!q.empty()) {
-        int now = q.front();
-        q.pop();
+    while (!pq.empty()) {
+        int now = pq.top();
+        pq.pop();
         if (visited[now]) continue;
         res.push_back(now+1);
         for (int edge: graph[now]) {
-            if (--(in_degree[edge]) == 0) q.push(edge);
+            if (--(in_degree[edge]) == 0) pq.push(edge);
         }
     }
-    if (res.size() == N) for (int i: res) cout<<i<<endl;
-    else cout<<0<<endl;
+    for (int i: res) cout<<i<<' ';
+    // if (res.size() == N) for (int i: res) cout<<i<<endl;
+    //else cout<<0<<endl;
 }
 
 int main() {
