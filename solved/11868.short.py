3df221c16572d60e840824fe22a59fdbf1cd9116
CodingNoye <codingnoye@kookmin.ac.kr>
2022-04-09 02:46:14 +0900

    0409

---
+++ solved/11868.short.py
@@ -0,0 +1,2 @@
+a,b=open(0)
+print('ckuoboeslaogvae r'[eval(b.replace(' ','^'))>0::2])
---
+++ solved/18937.py
@@ -0,0 +1,4 @@
+g=0
+a,b,c=open(0)
+for v in b.split():g^=int(v)-2
+print('BWlhaictke'[(c<'C')^(g>0)::2]+'king')
